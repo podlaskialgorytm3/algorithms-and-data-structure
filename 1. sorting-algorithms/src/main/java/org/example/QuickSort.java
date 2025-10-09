e1f0b69f6636012a1746ad8f7938f01d162809bb
podlaskialgorytm3 <live.m851@gmail.com>
2025-10-09 13:13:16 +0200

    feat: Create new common interfface

---
+++ 1. sorting-algorithms/src/main/java/org/example/QuickSort.java
@@ -0,0 +1,39 @@
+package org.example.Sorter;
+
+import org.example.Sorter;
+
+public class QuickSort implements Sorter {
+    @Override
+    public void sort(Double [] data){
+        if(data == null || data.length == 0){
+            return;
+        }
+        quickSort(data,0,data.length-1);
+    }
+    private void quickSort(Double [] data, int left, int right){
+        if(left < right){
+            int pivotIndex =  partition(data, left, right);
+
+            quickSort(data, left, pivotIndex-1);
+            quickSort(data, pivotIndex+1, right);
+        }
+    }
+    private int partition(Double [] data, int left, int right){
+            double pivot = data[right];
+            int i = left - 1;
+
+            for(int j = left; j < right; j++){
+                if(data[j] <= pivot){
+                    i++;
+                    replace(data[i], data[j]);
+                }
+            }
+            replace(data[i], data[right]);
+            return i+1;
+    }
+    private void replace(Double element1, Double element2){
+        double temp = element1;
+        element1 = element2;
+        element2 = temp;
+    }
+}
---
+++ 1. sorting-algorithms/src/main/java/org/example/Sorter.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface Sorter {
+    void sort(Double [] data);
+}
